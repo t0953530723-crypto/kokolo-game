963a5e754ddcd5f29db011327e5ffc8eed83b056
t0953530723-crypto <t0953530723@gmail.com>
2026-02-20 03:37:33 +0800

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-    base: './', // 使用相對路徑，確保 GitHub Pages 子目錄能正確讀取資源
-    build: {
-        assetsInlineLimit: 0,
-        chunkSizeWarningLimit: 1000,
-    }
-});
